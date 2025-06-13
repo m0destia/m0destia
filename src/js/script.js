3b06e1e0adff77178d0de840362d3885c64f1d94
Vinícius <viniciussz1470@outlook.com>
2025-06-13 20:12:12 -0300

    muchas cosas

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,33 @@
 const weatherapp = "https://weatherapp-1kaq.onrender.com/";
 const stphdesign = "https://stphdesigner.vercel.app/";
+const hiperprice = "https://hiperprice-supermercado.vercel.app/";
+
+const weatherappCard = document.getElementById("weather_app");
+const stphdesignCard = document.getElementById("stphdesigner");
+const hiperpriceCard = document.getElementById("hiperprice");
+
+const projectButton = document.getElementById("project_button");
+const contractButton = document.getElementById("contract_button");
+
+const projects = document.getElementById("projects");
+const contact = document.getElementById("contact");
+
+projectButton.addEventListener("click", () => {
+  projects.scrollIntoView({ behavior: "smooth" });
+});
+
+contractButton.addEventListener("click", () => {
+  contact.scrollIntoView({ behavior: "smooth" });
+});
+
+weatherappCard.addEventListener("click", () => {
+  window.open(weatherapp, "_blank").focus();
+});
+
+stphdesignCard.addEventListener("click", () => {
+  window.open(stphdesign, "_blank").focus();
+});
+
+hiperpriceCard.addEventListener("click", () => {
+  window.open(hiperprice, "_blank").focus();
+});
